11348f835c5e72cf49b6e6101bf1d29b4d697ac0
Andrew Vu <avu@redhat.com>
2022-08-02 17:44:10 -0500

    updated start-dci-runner.sh

--- start-dci-container-runner.sh
+++ start-dci-container-runner.sh
@@ -92,7 +92,7 @@ logerr() {
 #    exit 0
 #fi
 
-# Checks whether files or directories exist from David's script
+# Next 3 funcs got from David's script
 file_exists() {
         [ -z "${1-}" ] && bye Usage: file_exists name.
         ls "$1" >/dev/null 2>&1
@@ -108,7 +108,7 @@ log() {
         printf '%s %s\n' "$(date +"%Y%m%d-%H:%M:%S")" "$*"
 }
 
-##Add checking if these 4 files are present before start copy to the pods##
+# Add checking if these 4 files are present before start copy to the pods
 case $TEST_TYPE in
     CHART)
         file_exists "auth.json" || bye "$i: No such file."
@@ -121,13 +121,13 @@ case $TEST_TYPE in
     *) ;;
 esac
 
-#Additional Checking for general files existing or not#
+# Additional Checking for general files existing or not
 for i  in settings.yml dcirc.sh install.yml kubeconfig dci-runner.sh
 do
       file_exists "$i" || bye "$i: No such file."
 done
 
-#Get CNF name from dci-runner.sh#
+# Get CNF name from dci-runner.sh
 CNF_NAME=$(grep 'CNF=' dci-runner.sh |cut -d= -f2)
 if [[ "$SKIP_COPY" != +(yes|YES) ]]; then
       case $TEST_TYPE in
@@ -140,12 +140,10 @@ if [[ "$SKIP_COPY" != +(yes|YES) ]]; then
              logmsg "Copying pyxis-apikey.txt and auth.json files to ${POD_NAME}:/var/lib/dci-openshift-app-agent"
              oc -n $NAMESPACE cp auth.json  ${POD_NAME}:/var/lib/dci-openshift-app-agent/auth.json
              oc -n $NAMESPACE cp pyxis-apikey.txt  ${POD_NAME}:/var/lib/dci-openshift-app-agent/pyxis-apikey.txt
-             # Why remove helm-chart-xx.yml? cuz dci-runner.sh will check if this file existed, then it will run helm chart also with other test types
-             oc -n $NAMESPACE exec -it ${POD_NAME}  -- bash -c "rm /var/lib/dci-openshift-app-agent/helm-charts-${CNF_NAME}.yml" >/dev/null 2>&1
              ;;
          *) ;;
      esac
-     logmsg "Copying settings.yml, install.yml, dci-runner.sh, dcirc.sh and kubeconfig to ${POD_NAME}:/etc/dci-openshift-app-agent/"
+     logmsg "Copying settings.yml, install.yml, dci-runner.sh, dcirc.sh and kubeconfig to ${POD_NAME}"
      oc -n $NAMESPACE cp settings.yml ${POD_NAME}:/etc/dci-openshift-app-agent/
      oc -n $NAMESPACE cp dcirc.sh ${POD_NAME}:/etc/dci-openshift-app-agent/
      oc -n $NAMESPACE cp install.yml  ${POD_NAME}:/etc/dci-openshift-app-agent/hooks/
@@ -154,6 +152,15 @@ if [[ "$SKIP_COPY" != +(yes|YES) ]]; then
      oc -n $NAMESPACE exec -it ${POD_NAME}  -- bash -c 'sudo chown -R dci-openshift-app-agent:dci-openshift-app-agent /var/lib/dci-openshift-app-agent/*'
 fi
 
-##Start run dci-agent test remotely from your laptop/other nodes##
-logmsg "Start DCI Container Runner for ${TEST_TYPE} Testing Type...."
+if [[ "$TEST_TYPE" == +(TNF|PREFLIGHT) ]]; then
+    # Why remove helm-chart-xx.yml? cuz dci-runner.sh will check if this file existed, then it will run helm chart also with other tests
+    logmsg "Removing /var/lib/dci-openshift-app-agent/helm-charts-${CNF_NAME}.yml when not test Chart-Verifier"
+    oc -n $NAMESPACE exec -it ${POD_NAME}  -- bash -c "rm /var/lib/dci-openshift-app-agent/helm-charts-${CNF_NAME}.yml" >/dev/null 2>&1
+fi
+
+# Clear the dnf cache for Repo Download Issue(Not Fatal) when run dci-runner.sh
+oc -n $NAMESPACE exec -it ${POD_NAME}  -- bash -c 'sudo rm -rf /var/cache/dnf'
+
+# Start run dci-agent test remotely from your laptop/other nodes
+logmsg "Start DCI Container Runner for ${TEST_TYPE} Test-Type..."
 oc -n $NAMESPACE exec -it ${POD_NAME} -- bash -c 'su - dci-openshift-app-agent -c "export KUBECONFIG=/var/lib/dci-openshift-app-agent/kubeconfig; bash /var/lib/dci-openshift-app-agent/dci-runner.sh"'
